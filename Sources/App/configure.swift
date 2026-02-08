b249e04664995ce6398c9b3f96c2fc6350b5f906
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2026-02-08 13:47:41 -0500

    Initialize template

---
+++ Sources/App/configure.swift
@@ -0,0 +1,67 @@
+import LibP2P
+%%IMPORT%%
+
+// configures your application
+public func configure(_ app: Application) async throws {
+    
+    // We can specify the global log level here
+    app.logger.logLevel = .notice
+
+    // Install our modules on libp2p
+    %%INSTALLATION%%
+    
+    // Configure the modules to be used
+    %%POST_INSTALLATION%%
+    
+    // Add a custom command
+    app.asyncCommands.use(Cowsay(), as: "cowsay")
+    
+    // register routes
+    try routes(app)
+    
+    app.eventLoopGroup.next().scheduleTask(in: .milliseconds(100)) {
+        for address in app.listenAddresses {
+            let fullAddress = try address.encapsulate(proto: .p2p, address: app.peerID.b58String)
+            app.logger.notice("Libp2p listening at \(fullAddress)")
+        }
+    }
+}
+
+/// An example of a custom command you can add to your app
+///
+/// Execute the `cowsay` cmd by running
+/// ```
+/// swift run App cowsay "Mmooo" --eyes "👀" --tongue "👅"
+/// ```
+struct Cowsay: AsyncCommand {
+    struct Signature: CommandSignature {
+        @Argument(name: "message")
+        var message: String
+
+        @Option(name: "eyes", short: "e")
+        var eyes: String?
+
+        @Option(name: "tongue", short: "t")
+        var tongue: String?
+    }
+
+    var help: String {
+        "Generates ASCII picture of a cow with a message."
+    }
+
+    func run(using context: CommandContext, signature: Signature) async throws {
+        let eyes = signature.eyes ?? "oo"
+        let tongue = signature.tongue ?? "  "
+        let cow = #"""
+          < $M >
+                  \   ^__^
+                   \  ($E)\_______
+                      (__)\       )\/\
+                       $T ||----w |
+                          ||     ||
+        """#.replacingOccurrences(of: "$M", with: signature.message)
+            .replacingOccurrences(of: "$E", with: eyes)
+            .replacingOccurrences(of: "$T", with: tongue)
+        context.console.print(cow)
+    }
+}
